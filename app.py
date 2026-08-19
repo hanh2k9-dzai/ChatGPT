e2b6e8382cc479ddb26edc60f2fdf0f59d54c52c
lehanh <taovanluc45@gmail.com>
2026-08-19 19:00:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+client = OpenAI(api_key=os.environ["sk-AJy-g5xdTiBPGoM7pIq3bA"])
 
 print("=== MY AI ===")
 
